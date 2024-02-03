abf0af82fb210173ef0967b7b6fb012d0e58a3aa
techwithguga <guramxatiashvili@hotmail.com>
2024-02-03 10:12:13 +0000

    deal function to get random card from the deck

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+import random
+
+def deal_card():
+  """Returns a random card from the deck."""
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  return card
+
+random_card=deal_card()
+print(random_card)
+
+    
